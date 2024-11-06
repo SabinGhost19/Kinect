1e0e54a3708ccee3a2a1ff7586cd20e6c1f7436d
Sabin <sabinstan19@gmail.com>
2024-11-06 19:29:28 +0200

    down MongoDB container from DockerHub, up and running

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,24 @@
-function funct(name: string): number {
-  if (name == "Sab") {
-    return 1;
+const express = require("express");
+const app = express();
+require("dotenv").config({ path: "./src/.env" });
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use(cors());
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("Connected to the MongoDB!!!!!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
   }
-  return 0;
-}
+};
 
-const ret: number = funct("Sab");
-
-console.log(ret);
+connectToMongo();
+app.listen(process.env.PORT, () => {
+  console.log("Server up and running...");
+  console.log(process.env.MONGODB_URI);
+});
